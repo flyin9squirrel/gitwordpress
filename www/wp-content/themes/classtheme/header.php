e6a8c3ca0ff4a1e3ec91f13e09cc79aae857663f
anorris <flyin9squirrel@gmail.com>
2014-06-26 09:56:55 -0400

    Added Cloudinary, modified htaccess

--- www/wp-content/themes/classtheme/header.php
+++ www/wp-content/themes/classtheme/header.php
@@ -1,11 +1,13 @@
-<!Doctype html>
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<title>Tutorial theme</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Flying Squirrel</title>
+<link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/css/lavish-bootstrap.css" />
 <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
 </head>
 <body>
-	<div id="wrapper">
+	<div id="wrapper" class="container">
 		<div id="header">
-			<h1>HEADER</h1>
+			<img src="<?php echo get_template_directory_uri() ?>/images/flyingsquirrelbanner.jpg" class="img-responsive" alt="Flying Squirrel">
 		</div>
